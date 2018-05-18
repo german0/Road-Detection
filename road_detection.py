e3ff9d9c1d1ebca0aa1caf013357b934af9853e4
Germano <a71150@alunos.uminho.pt>
2018-05-19 00:14:47 +0100

    meh

--- road_detection.py
+++ road_detection.py
@@ -1,15 +1,13 @@
-import rasterio
-import geopyspark as gps
 import numpy as np
 import gdal
 import cv2
 import matplotlib.pyplot as plt
+import mahotas
 
 from os import walk
-from scipy.misc import imsave
 
 default_path = "R10_test"
-
+''''
 def read_image(path):
     bandas = []
     tci = []
@@ -53,10 +51,10 @@ def detect_clouds(image,N,L):
     return image
 
 #ajuste de iluminação da imagem
-def adjust_gamma(image, gamma=0.5):
+def adjust_gamma(image, gamma=0.3):
     # build a lookup table mapping the pixel values [0, 255] to
     # their adjusted gamma values
-    invGamma = 1.0 / gamma
+    invGamma = 1.5 / gamma
     table = np.array([((i / 255.0) ** invGamma) * 255
     for i in np.arange(0, 256)]).astype("uint8")
     # apply gamma correction using the lookup table
@@ -76,10 +74,10 @@ def print_image(image):
 
 def build_filters():
     filters = []
-    ksize = 21
+    ksize = 52
     for theta in np.arange(0, np.pi, np.pi / 7):
         #kern = cv2.getGaborKernel((ksize, ksize), 1.0, theta, 5.0, 0.5, 0, ktype=cv2.CV_32F)
-        kern = cv2.getGaborKernel((ksize,ksize), 1.0, theta, 5.0, 0.5, 0, ktype=cv2.CV_32F)
+        kern = cv2.getGaborKernel((ksize,ksize), 3.0, theta, 10.0, 0.5, 0, ktype=cv2.CV_32F)
         kern /= 1.5*kern.sum()
         filters.append(kern)
     return filters
@@ -93,11 +91,10 @@ def process_gabor(img, filters):
     return accum
 
 def gabor_filtering(image):
-    print_image(image)
     filters = build_filters()
     gabor = process_gabor(image,filters)
-    print_image(gabor)
-    ret, th1 = cv2.threshold(gabor, 127, 255, cv2.THRESH_BINARY)
+    #print_image(gabor)
+    ret, th1 = cv2.threshold(image, 100, 255, cv2.THRESH_BINARY)
     print_image(th1)
     return gabor
 
@@ -110,7 +107,90 @@ def path_opening(image):
     erode = cv2.erode(th1,kernel)
     dilation = cv2.dilate(erode,kernel,iterations=1)
     print_image(image)
-    return 0
+    return 0'''
+
+def print_image(image):
+    plt.imshow(image,cmap='gray')
+    plt.show()
+
+def crop(image):
+    image = np.array(image)
+    N,L = image.shape
+    step_x = round(N/2)
+    step_y= round(L/2)
+    for x in range(0,N-step_x,step_x):
+        for y in range(0,L-step_y,step_y):
+            cropped = image[x:x+step_x,y:y+step_y]
+            print_image(cropped)
+
+def detect_clouds(image):
+    N,L = image.shape
+    blur = cv2.GaussianBlur(image, (5, 5), 0)
+    mean = np.array(blur).mean()
+    st = np.array(blur).std()
+    for i in range(0,N):
+        for j in range(0,L):
+            if image[i,j] <= mean - st or image[i,j] >= mean+st:
+                image[i,j] = 0
+    return image
+
+#ajuste de iluminação da imagem
+def adjust_gamma(image, gamma=0.5):
+    # build a lookup table mapping the pixel values [0, 255] to
+    # their adjusted gamma values
+    invGamma = 1.5 / gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255
+    for i in np.arange(0, 256)]).astype("uint8")
+    # apply gamma correction using the lookup table
+    return cv2.LUT(image, table)
+
+def pre_process(tci):
+    final = tci
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    contrast = clahe.apply(final)
+    gamma = adjust_gamma(contrast)
+    return gamma
+
+def process_gabor(img, filters):
+    accum = np.zeros_like(img)
+    gabor = img
+    for kern in filters:
+        fimg = cv2.filter2D(gabor, cv2.CV_8UC3, kern)
+        np.maximum(accum, fimg, accum)
+    return accum
+
+def build_filters():
+    filters = []
+    ksize = 52
+    for theta in np.arange(0, np.pi, np.pi / 7):
+        #kern = cv2.getGaborKernel((ksize, ksize), 1.0, theta, 5.0, 0.5, 0, ktype=cv2.CV_32F)
+        kern = cv2.getGaborKernel((ksize,ksize), 3.0, theta, 10.0, 0.5, 0, ktype=cv2.CV_32F)
+        kern /= 1.5*kern.sum()
+        filters.append(kern)
+    return filters
+
+def divide_regions(image):
+    division = 4
+    step = round(255/division)
+    mean = np.array(image).mean()
+    st = np.array(image).std()
+    region1 = image<step
+    region2 = image<(2*step)-region1
+    region3 = image<(3*step)-region2
+    region4 = image - region3
+    return region1,region2,region3,region4
+
+def gabor_filtering(image):
+    #ret, th1 = cv2.threshold(image, 40, 255, cv2.THRESH_BINARY)
+    #opening = cv2.morphologyEx(th1, cv2.MORPH_OPEN, kernel)
+    #filters = build_filters()
+    #gabor = process_gabor(image,filters)
+    #print_image(gabor)
+    #ret, th1 = cv2.threshold(image.astype(np.uint8), 40, 255, cv2.THRESH_BINARY)
+    #print_image(th1)
+    print_image(cv2.Canny(image.astype(np.uint8),100,200))
+    #return gabor
+
 
 if __name__=="__main__":
     #path = ""
@@ -122,12 +202,22 @@ if __name__=="__main__":
     #obter images
     #tci = read_image(path)
 
-    tci = (cv2.imread('wtf.tif'))[:,:,1]
-    N,L = tci.shape
-    mean = detect_clouds(tci,N,L)
-    pre = pre_process(mean)
-    gabor = gabor_filtering(pre)
-    #opening = path_opening(gabor)
-    #opening = path_opening(gabor)
-    #imsave("teste.jpeg", tci)
+    tci = np.array((cv2.imread('wtf.tif'))[:,:,1])
+    #print_image(tci)
+    pre = pre_process(tci)
+    r1,r2,r3,r4 = divide_regions(pre)
+    clouds = r3 * pre
+    grass = (1-r1) * clouds
+    #cv2.Canny(grass.astype(np.uint8),0,255)
+    #gabor_filtering(np.array(grass))
+    #print_image(pre)
+    f = plt.figure()
+    f.add_subplot(1, 3, 1)
+    plt.imshow(pre)
+    f.add_subplot(1, 3, 2)
+    plt.imshow(grass)
+    f.add_subplot(1, 3, 3)
+    plt.imshow(cv2.morphologyEx(grass.astype(np.uint8), cv2.MORPH_OPEN, np.ones((2,2)),iterations=2))
+    plt.show()
+    #gabor_filtering(new)
 
