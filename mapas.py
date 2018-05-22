e1550b19d425a429c8addd4c8fb0dc12bf30806b
Germano <a71150@alunos.uminho.pt>
2018-05-22 10:47:57 +0100

    road skeleton

--- mapas.py
+++ mapas.py
@@ -7,8 +7,19 @@ import mahotas
 from os import walk
 default_path = "R10_test"
 
+def mean(image):
+    N,L = image.shape
+    total = 0
+    values = 0
+    for x in range(N):
+        for y in range(L):
+            if image[x,y] > 0:
+                values += image[x,y]
+                total +=1
+    return values/total
+
 def adaptative_thresholding(image):
-    m = image.mean()
+    m = np.array(image).mean()
     th1 = round(m/2)
     th2 = m + round(m/2)
     th3 = 255 - round(m/2)
@@ -18,14 +29,13 @@ def adaptative_thresholding(image):
     mask_d = image > th2
     mask = np.bitwise_or(mask_c, mask_d)
     road = image * mask
-    image = road
-    return image
+    return np.array(mask,dtype="uint8")
 
 def compare_images(image1, image2):
     f = plt.figure()
-    f.add_subplot(1, 3, 1)
+    f.add_subplot(1, 2, 1)
     plt.imshow(image1,cmap='gray')
-    f.add_subplot(1, 3, 2)
+    f.add_subplot(1, 2, 2)
     plt.imshow(image2,cmap='gray')
     plt.show()
 
@@ -39,14 +49,76 @@ def adjust_gamma(image, gamma=0.3):
     # apply gamma correction using the lookup table
     return cv2.LUT(image, table)
 
-def pre_process(tci):
-    final = tci
+def pre_process(image):
+    final = image
     #subdividir a imagem de forma a que o aumento do contraste tenha melhores resultados
     clahe = cv2.createCLAHE(clipLimit=4.0, tileGridSize=(8,8))
     contrast = clahe.apply(final)
-    compare_images(final,contrast)
     gamma = adjust_gamma(contrast)
     return gamma
+
+def neighbours_vec(image):
+    return image[2:,1:-1], image[2:,2:], image[1:-1,2:], image[:-2,2:], image[:-2,1:-1], image[:-2,:-2], image[1:-1,:-2], image[2:,:-2]
+
+def transitions_vec(P2, P3, P4, P5, P6, P7, P8, P9):
+    return ((P3-P2) > 0).astype(int) + ((P4-P3) > 0).astype(int) +\
+           ((P5-P4) > 0).astype(int) + ((P6-P5) > 0).astype(int) +\
+           ((P7-P6) > 0).astype(int) + ((P8-P7) > 0).astype(int) +\
+           ((P9-P8) > 0).astype(int) + ((P2-P9) > 0).astype(int)
+
+def zhangSuen_vec(image, iterations):
+    for iter in range (1, iterations):
+        print (iter)
+        # step 1
+        P2,P3,P4,P5,P6,P7,P8,P9 = neighbours_vec(image)
+        condition0 = image[1:-1,1:-1]
+        condition4 = P4*P6*P8
+        condition3 = P2*P4*P6
+        condition2 = transitions_vec(P2, P3, P4, P5, P6, P7, P8, P9) == 1
+        condition1 = (2 <= P2+P3+P4+P5+P6+P7+P8+P9) * (P2+P3+P4+P5+P6+P7+P8+P9 <= 6)
+        cond = (condition0 == 1) * (condition4 == 0) * (condition3 == 0) * (condition2 == 1) * (condition1 == 1)
+        changing1 = np.where(cond == 1)
+        image[changing1[0]+1,changing1[1]+1] = 0
+        # step 2
+        P2,P3,P4,P5,P6,P7,P8,P9 = neighbours_vec(image)
+        condition0 = image[1:-1,1:-1]
+        condition4 = P2*P6*P8
+        condition3 = P2*P4*P8
+        condition2 = transitions_vec(P2, P3, P4, P5, P6, P7, P8, P9) == 1
+        condition1 = (2 <= P2+P3+P4+P5+P6+P7+P8+P9) * (P2+P3+P4+P5+P6+P7+P8+P9 <= 6)
+        cond = (condition0 == 1) * (condition4 == 0) * (condition3 == 0) * (condition2 == 1) * (condition1 == 1)
+        changing2 = np.where(cond == 1)
+        image[changing2[0]+1,changing2[1]+1] = 0
+    return image
+
+def skeleton(img):
+    size = np.size(img)
+    skel = np.zeros(img.shape, np.uint8)
+    element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+    done = False
+
+    while (not done):
+        eroded = cv2.erode(img, element)
+        temp = cv2.dilate(eroded, element)
+        temp = cv2.subtract(img, temp)
+        skel = cv2.bitwise_or(skel, temp)
+        img = eroded.copy()
+
+        zeros = size - cv2.countNonZero(img)
+        print(size,zeros)
+        if zeros == size:
+            done = True
+    return skel
+
+def process(image):
+    kernel1 = np.ones((8, 8), np.uint8)
+    kernel2 = np.ones((2, 2), np.uint8)
+    m_opening = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel1)
+    m_opening = m_opening > 0
+    opening = np.array(image * (1-m_opening),dtype="uint8")
+    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel2)
+    skel = skeleton(opening)
+    return skel
 
 if __name__=="__main__":
     #path = ""
@@ -57,13 +129,16 @@ if __name__=="__main__":
 
     #obter images
     #tci = read_image(path)
+    kernel1 = np.ones((8, 8), np.uint8)
+    kernel2 = np.ones((3, 3), np.uint8)
+    kernel3 = cv2.getStructuringElement(cv2.MORPH_CROSS, (8, 8))
+    kernel4 = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
 
     tci = np.array((cv2.imread('wtf.tif'))[:,:,1])
     image = np.array(tci)
     pre = pre_process(image)
     road = adaptative_thresholding(pre)
-    compare_images(image,road)
+    skel = process(road)
+    compare_images(image,skel)
     #teste = pre_process(road)
-    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (5, 5))
-    kernel2 = cv2.getStructuringElement(cv2.MORPH_RECT,(2,2))
-    closing = cv2.morphologyEx(road, cv2.MORPH_CLOSE, kernel2)
+    #compare_images(road,closing)
